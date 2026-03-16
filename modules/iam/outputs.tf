de70b59ce79121599bc0cfc10fe44e19550fba2b
Diego Liascovich <padie78@gmail.com>
2026-03-16 12:11:44 +0200

    fix duplicated

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -1,13 +1,6 @@
 # ==========================================
 # IAM MODULE OUTPUTS
 # ==========================================
-
-output "cluster_role_arn" {
-  description = "ARN of the EKS Cluster IAM Role"
-  # Acceso directo a la clave del mapa generado por for_each
-  value       = try(aws_iam_role.roles["cluster"].arn, null)
-}
-
 output "cluster_role_arn" {
   value = aws_iam_role.roles["cluster"].arn
 }
